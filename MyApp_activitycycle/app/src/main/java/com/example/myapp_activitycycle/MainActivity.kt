c7f34aa9c5bca8a287e3fad448eb03a44f350001
ShaluuS07 <shalinisaravanan.s@caratlane.com>
2023-01-09 14:22:02 +0530

    first commit

---
+++ MyApp_activitycycle/app/src/main/java/com/example/myapp_activitycycle/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myapp_activitycycle
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+ val TAG = "Activity 1"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
+        val btn_next = findViewById<Button>(R.id.btn_next)
+        btn_next.setOnClickListener {
+            val i = Intent(this, MainActivity2::class.java)
+            startActivity(i)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+}
